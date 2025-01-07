4d910dc445a96a6f3cd8c99e7f526c673edf21b8
kawtharnasser <kawtherhabib.ko@gmail.com>
2025-01-07 23:03:06 +0300

    completed now

--- challenge3.py
+++ challenge3.py
@@ -7,20 +7,8 @@ occurances('fleep floop', 'p')   # returns 2
 occurances('fleep floop', 'ee')  # returns 1
 occurances('fleep floop', 'fe')  # returns 0
 '''
-def occurances(word,letters):
-    count=0
-
-    if len(letters) ==2:
-        for ltr in range(3):
-            if(word[ltr:ltr + 2] == letters): #take the letter and the character after it "from the stack overflow (sourse)[https://stackoverflow.com/questions/1162592/iterate-over-a-string-2-or-n-characters-at-a-time-in-python]"
-                count +=1
-                break
-
-
-    for ltr in word:
-        if(ltr==letters):
-            count+=1
-    print(count)
+def occurances(word, letters):
+    print(word.count(letters))
 
 
 occurances('fleep floop', 'e')   # returns 2
